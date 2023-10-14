767d109ce668c4c5d6e7665ceef87c4c4b760b55
Tasif1 <96546720+Tasif1@users.noreply.github.com>
2023-10-14 15:28:39 -0400

    Updated Choose locations page

--- myweekend/src/pages/ChooseLocation.jsx
+++ myweekend/src/pages/ChooseLocation.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import Typewriter from './components/Typewriter.jsx'
+import Rating from '@mui/material/Rating';
 
 import logo from '../assets/logo.png'
 
 function ChooseLocation() {
   const [locationIndex, setLocationIndex] = useState(0);
   const [chosenLocations, setChosenLocations] = useState([]);
+  const navigate = useNavigate();
 
   const locations = [
     {
@@ -14,19 +17,23 @@ function ChooseLocation() {
       name: "Canada's Wonderland",
       description: "Canada's Wonderland is a 134-hectare theme park located in Vaughan, Ontario, a suburb approximately 25 kilometres north of Downtown Toronto. Opened in 1981 by the Taft Broadcasting Company and The Great-West Life Assurance Company as the first major theme park in Canada, it remains the country's largest.",
       picture: "https://viewthevibe.com/wp-content/uploads/2021/08/Screen-Shot-2021-08-13-at-9.44.49-AM.png",
-      price: 45
+      price: 45,
+      rating: 4.4,
+      hours: "10:00 AM - 8:00 PM"
     },
     {
       id: 2,
       name: "CN Tower",
       description: "The CN Tower is a 553.3 m-high concrete communications and observation tower located in Downtown Toronto, Ontario, Canada. Built on the former Railway Lands, it was completed in 1976. Its name CN originally referred to Canadian National, the railway company that built the tower.",
       picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Toronto_-_ON_-_Toronto_Harbourfront7.jpg/1200px-Toronto_-_ON_-_Toronto_Harbourfront7.jpg",
-      price: 50
+      price: 50,
+      rating: 4.6,
+      hours: "9:00 AM - 10:00 PM"
     }
   ]
 
   function handleSubmit() {
-    console.log(chosenLocations);
+    navigate('/localresult');
   }
 
   return (
@@ -56,11 +63,18 @@ function ChooseLocation() {
                     className="h-full w-full object-cover object-center"
                   />
                 </div>
-                <div className="p-4">
+                <div className="p-4 space-y-1">
                   <h3 className="text-lg font-semibold line-clamp-1">{locations[locationIndex].name}</h3>
+                  <div className="flex flex-row items-center space-x-1">
+                    <p className="text-md font-medium text-gray-900">{locations[locationIndex].rating}</p>
+                    <Rating name="half-rating-read" value={locations[locationIndex].rating} precision={0.5} readOnly />
+                  </div>
                   <p className="text-sm text-gray-500 line-clamp-2">{locations[locationIndex].description}</p>
                   <div className="flex items-center justify-between pt-4">
-                    <p className="text-md font-medium text-gray-900">$ {locations[locationIndex].price}</p>
+                    <div className="flex flex-row items-center space-x-4">
+                      <p className="text-md font-medium text-gray-900">$ {locations[locationIndex].price}</p>
+                      <p className="text-sm font-medium text-gray-900">{locations[locationIndex].hours}</p>
+                    </div>
                     {
                       chosenLocations.some((location) => location.id === locations[locationIndex].id) ? (
                         <button className="rounded-full h-8 w-8 bg-red-500 flex items-center justify-center hover:bg-red-600"
@@ -91,15 +105,18 @@ function ChooseLocation() {
             {chosenLocations.length > 0 && (
               <div>
                 <p className="text-lg font-semibold mt-10 text-white p-4">Locations to include</p>
-                <div className="w-full h-40 p-2 overflow-y-auto space-y-4">
+                <div className="w-full h-32 px-2 overflow-y-auto space-y-4">
                   {
                     chosenLocations.map((location) => (
                       <div className="w-full h-32 bg-white flex flex-row items-center space-x-4 pr-4 rounded-xl overflow-clip">
                         <img src={location.picture} alt={location.name} className="h-32 w-32 flex-shrink-0 object-cover object-center" />
-                        <div>
+                        <div className="space-y-1">
                           <p className="text-lg font-semibold line-clamp-1">{location.name}</p>
                           <p className="text-sm text-gray-500 line-clamp-2">{location.description}</p>
-                          <p className="text-md font-medium text-gray-900">$ {location.price}</p>
+                          <div className="flex flex-row items-center space-x-4">
+                            <p className="text-md font-medium text-gray-900">$ {location.price}</p>
+                            <p className="text-sm font-medium text-gray-900">{location.hours}</p>
+                          </div>
                         </div>
                         <button className="rounded-full h-8 w-8 flex-shrink-0 bg-red-500 flex items-center justify-center hover:bg-red-600"
                           onClick={() => { setChosenLocations(chosenLocations.filter((loc) => loc.id !== location.id)) }}>
@@ -114,16 +131,17 @@ function ChooseLocation() {
               </div>
             )}
           </div>
-          <div className="w-1/2 flex items-center justify-center text-white">
-            Map
+          <div className="w-1/2 flex text-white px-8 pb-8">
+            <iframe src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyCAao5fwt_7o0UNWFHSsi6mMkljPpsHSdU&q=" + locations[locationIndex].name.split(' ').join('+') + ",Toronto+ON"} 
+              className='w-full h-full rounded-xl' />
           </div>
         </div>
       </div>
-      <div className="flex justify-center p-4">
+      <div className="flex justify-center pb-4">
         <button className="w-64 bg-logopink rounded-lg p-4 text-white font-semibold text-lg hover:bg-altlogopink"
           onClick={handleSubmit}>
           Create Trip
-          </button>
+        </button>
       </div>
     </div>
   )
